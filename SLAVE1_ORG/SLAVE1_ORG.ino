80059ccb3120f9e7b5bab87dc971322bf483f36d
NemzNemz <urgot0933@gmail.com>
2025-02-24 22:32:42 +0700

    Khac phuc loi ngat UART

--- SLAVE1_ORG/SLAVE1_ORG.ino
+++ SLAVE1_ORG/SLAVE1_ORG.ino
@@ -2,7 +2,9 @@
 #include <WiFi.h>
 #include "Sensor_data.h"
 #include "GG_SHEET.h"
+//Thu vien cua Espressif
 #include "driver/uart.h"
+#include "freertos/queue.h" 
 
 //Khai bao bien peripheral ten la pin
 unsigned long prev_main = 0;
@@ -15,52 +17,14 @@ typedef struct SLAVE_CFG{
 }Slave;
 Slave slv;
 
-const uart_port_t uart_num = UART_NUM_1;
-
-//esp-idf/components/hal/esp32/include/haluart_ll.h
-#define UART_RXFIFO_FULL_INT_CLR (0x01<<0)
-#define UART_RXFIFO_TOUT_INT_CLR (0x01<<8)
-const int rx_buffer_size = 1024;
-const int tx_buffer_size = 1024;
-const int queue_size = 10;
-
-uart_config_t uart_config{
-  .baud_rate = slv.BAUD_RATE,
-  .data_bits = UART_DATA_8_BITS,
-  .parity = UART_PARITY_DISABLE,
-  .stop_bits = UART_STOP_BITS_1,
-  .flow_ctrl = UART_HW_FLOWCTRL_DISABLE,
-  // EXTEND UART_CONFIG_T
-  //.rx_flow_ctrl_thresh = 122,
-  // .source_clk =
-  // .backup_before_sleep =
-  // .flags = {.enable = true, .low_power_mode = false}
-};
-
-//Ham ngat prototype
-static void IRAM_ATTR uart_intr_handle(void *arg){
-  //6 bit vua du cho "SLV_01"
-  uint8_t buf[7];
-  size_t rx_fifo_len;
-
-  uart_get_buffered_data_len(uart_num, &rx_fifo_len);
-  //Neu data vua du SLV_01
-  if(rx_fifo_len >= 6){
-    uart_read_bytes(uart_num, buf, 6, 0);
-    buf[7] = '\0';
-    Serial.printf("RX Length: %d\n", rx_fifo_len);
-
-    if(strstr((char*)buf, slv.SLAVE_COMMAND)!=NULL){
-      //Gui data qua UART
-      char buffer[40];
-      snprintf(buffer, sizeof(buffer),"Temp_SL:%d\n", data.water_level);
-      uart_write_bytes(uart_num, buffer, strlen(buffer));
-    }
-  }
-  //Xoa co ngat
-  uart_flush(uart_num);
-  uart_clear_intr_status(uart_num, UART_RXFIFO_FULL_INT_CLR|UART_RXFIFO_TOUT_INT_CLR);
-}
+typedef struct uart_variable{
+  const uart_port_t uart_num = UART_NUM_1;
+  const int rx_buffer_size = 1024;
+  const int tx_buffer_size = 1024;
+  const int queue_size = 10;
+}uart_variable;
+uart_variable uart_var;
+QueueHandle_t uart_queue;
 
 void setup() {
   Serial.begin(slv.BAUD_RATE);
@@ -72,34 +36,56 @@ void setup() {
   Serial.println("\nWiFi Connected!");
   pin_cfg_init(&pin);
 
-  //Cau hinh UART
-  uart_param_config(uart_num, &uart_config);
-  //Cau hinh chan UART
-  uart_set_pin(uart_num, pin.ZIGBEE_TX, pin.ZIGBEE_RX, UART_PIN_NO_CHANGE, UART_PIN_NO_CHANGE);
-  //Cai dat driver
-  uart_driver_install(uart_num, rx_buffer_size, tx_buffer_size, queue_size, NULL, 0);
+  //Cau hinh UART 
+  uart_config_t uart_config{
+  .baud_rate = slv.BAUD_RATE,
+  .data_bits = UART_DATA_8_BITS,
+  .parity = UART_PARITY_DISABLE,
+  .stop_bits = UART_STOP_BITS_1,
+  .flow_ctrl = UART_HW_FLOWCTRL_DISABLE,
+  .source_clk = UART_SCLK_APB,
+  };
+  uart_param_config(uart_var.uart_num, &uart_config);
 
-  //Cau hinh ngat UART
-  uart_enable_rx_intr(uart_num);
-  //uart_isr_register ko xai duoc
-  gpio_isr_register(uart_intr_handle, NULL, ESP_INTR_FLAG_IRAM, NULL);
+  //Set pins
+  uart_set_pin(uart_var.uart_num, pin.ZIGBEE_TX, pin.ZIGBEE_RX, UART_PIN_NO_CHANGE, UART_PIN_NO_CHANGE);
+  //Cai driver UART
+  uart_driver_install(uart_var.uart_num, uart_var.rx_buffer_size, uart_var.tx_buffer_size, uart_var.queue_size, &uart_queue, 0);
 
   pinMode(pin.VCC_WTR, OUTPUT);
   pinMode(pin.WTR_PIN, INPUT);
 }
 
 void loop() {
-  unsigned long now_main = millis();
-  if(now_main - prev_main >= interval_main){
-    raw_wtr(&data);
-    water_lv(&data);
+  uart_event_t event;
+    if(xQueueReceive(uart_queue, &event, pdMS_TO_TICKS(300))){
+      if(event.type == UART_DATA){
+        char rx_buf[256];
+        int len = uart_read_bytes(uart_var.uart_num, rx_buf, event.size, 0);
+        //Ki tu ket thuc chuoi
+        rx_buf[len] = '\0';
 
-    // Serial.print("RAW ");
-    // Serial.println(data.raw_value_wtr);
-    // Serial.print("% ");
-    // Serial.println(data.water_level);
-    //sendDataToGoogleSheets();
+        if(strstr(rx_buf, slv.SLAVE_COMMAND) != NULL){
+          char buffer[40];
+          snprintf(buffer, sizeof(buffer), "WTR: %d\n", data.water_level);
+          uart_write_bytes(uart_var.uart_num, buffer, strlen(buffer));
+          //In log
+          Serial.println("Gui: "+String(buffer));
+        }
+      }
+    else Serial.println("No data yet");
 
-    prev_main = now_main;
+    unsigned long now_main = millis();
+    if(now_main - prev_main >= interval_main){
+      raw_wtr(&data);
+      water_lv(&data);
+
+      // Serial.print("RAW ");
+      // Serial.println(data.raw_value_wtr);
+      // Serial.print("% ");
+      // Serial.println(data.water_level);
+      // sendDataToGoogleSheets();
+      prev_main = now_main;
+    }
   }
 }
