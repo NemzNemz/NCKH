9ec61fa38ffe8b5205aa419ce0e79349342362d5
NemzNemz <urgot0933@gmail.com>
2025-03-06 13:51:10 +0700

    Them kha nang ket noi thanh cong Firebase nhung van con bug

--- MASTER_ORG/MASTER_ORG.ino
+++ MASTER_ORG/MASTER_ORG.ino
@@ -10,8 +10,8 @@
 #include <WiFi.h>
 
 //Config Firebase
-#define WIFI_SSID "Been"
-#define WIFI_PASS "11119999"
+#define WIFI_SSID "Trieu Ninh"
+#define WIFI_PASS "12344321"
 #define API_KEY "AIzaSyCNLfTrT6w3K2ipz9DBT198YocfGbZarII"
 #define DATABASE_URL "https://nckh-dd303-default-rtdb.firebaseio.com/"
 
@@ -31,6 +31,7 @@ uint16_t interval2 = 1700;
 uint8_t slave_id = 1;
 
 unsigned long prev_send_data = 0;
+uint16_t interval3 = 3000;
 bool signUP_OK = false;
 
 
@@ -46,8 +47,8 @@ bool signUP_OK = false;
 
 char buffer[40];
 int buf_index = 0;
-float lastValueSLV1 = 1;
-float lastValueSLV2 = 1;
+float lastValueSLV1 = 1.23;
+float lastValueSLV2 = 1.23;
 
 HardwareSerial zigbeeSerial(1); // ESP32: TX=17, RX=16 (Zigbee)
 
@@ -84,6 +85,7 @@ void setup() {
     config.database_url = DATABASE_URL;
     if(Firebase.signUp(&config, &auth, "", "")){
       Serial.println("Sign UP!");
+      signUP_OK = true;
     }
     else{
       Serial.printf("%s\n", config.signer.signupError.message.c_str());
@@ -94,55 +96,80 @@ void setup() {
     Firebase.reconnectWiFi(true);
 
     peripheral_init(&pin);
-    //zigbeeSerial.begin(115200, SERIAL_8N1, pin.ZIGBEE_RX, pin.ZIGBEE_TX);
+    zigbeeSerial.begin(115200, SERIAL_8N1, pin.ZIGBEE_RX, pin.ZIGBEE_TX);
     in_text_ra_lcd();
 }
 
-// void xu_ly_data(char* data){
-//   //tim chuoi con trong chuoi lon
-//   if(strncmp(data, "WTR: ", 5) == 0){
-//     char* valStr = data + 5;
-//     float value = atof(valStr);
+void xu_ly_data(char* data){
+  //tim chuoi con trong chuoi lon
+  if(strncmp(data, "WTR: ", 5) == 0){
+    char* valStr = data + 5;
+    float value = atof(valStr);
 
-//     if(slave_id == 1){
-//       if(value != lastValueSLV1){
-//         tft.setCursor(130, 35); 
-//         tft.print(value);
-//         lastValueSLV1 = value;
-//       }
-//     }
-//     else if(slave_id == 2){
-//       if(value != lastValueSLV2){
-//         tft.setCursor(130, 55); 
-//         tft.print(value);
-//         lastValueSLV2 = value;
-//       }
-//     }
-//   }
-// }
+    if(slave_id == 1){
+      if(value != lastValueSLV1){
+        tft.setCursor(130, 35); 
+        tft.print(value);
+        lastValueSLV1 = value;
+      }
+    }
+    else if(slave_id == 2){
+      if(value != lastValueSLV2){
+        tft.setCursor(130, 55); 
+        tft.print(value);
+        lastValueSLV2 = value;
+      }
+    }
+  }
+}
 
-// void nhan_data(){
-//   if(zigbeeSerial.available()){
-//    char c = zigbeeSerial.read();
-//     if(c == '\n'){
-//       buffer[buf_index] = '\0';
-//       //Serial.println("Nhan: " +String(buffer));
-//       unsigned long now2 = millis();
-//       if(now2 - prev2 > interval2){
-//       xu_ly_data(buffer);
-//       prev2 = now2;
-//       }
-//       buf_index = 0;
-//     }
-//     else{
-//       if(buf_index < sizeof(buffer) - 1){
-//         buffer[buf_index++] = c;
-//       }
-//     }
-//   }
-// }
+void nhan_data(){
+  if(zigbeeSerial.available()){
+   char c = zigbeeSerial.read();
+    if(c == '\n'){
+      buffer[buf_index] = '\0';
+      //Serial.println("Nhan: " +String(buffer));
+      unsigned long now2 = millis();
+      if(now2 - prev2 > interval2){
+      xu_ly_data(buffer);
+      prev2 = now2;
+      }
+      buf_index = 0;
+    }
+    else{
+      if(buf_index < sizeof(buffer) - 1){
+        buffer[buf_index++] = c;
+      }
+    }
+  }
+}
 
 void loop() {
-  //nhan_data();
-  //poll_id();
+  unsigned long now3 = millis();
+  if(now3 - prev_send_data > interval3){
+    nhan_data();
+    poll_id();
+    if(Firebase.ready() && signUP_OK == true){
+      float WTR_Value1 = lastValueSLV1;
+      float WTR_Value2 = lastValueSLV2;
+      if(Firebase.RTDB.setFloat(&fbdo, "Sensor/WTR_data1", WTR_Value1)){
+        Serial.println();
+        Serial.println("- thanh cong luu den: " + fbdo.dataPath());
+        Serial.println(" (" + fbdo.dataType() + ") ");
+      }
+      else {
+        Serial.println("That bai: " + fbdo.errorReason());
+      }
+
+      // if(Firebase.RTDB.setFloat(&fbdo, "Sensor/WTR_data2", WTR_Value2)){
+      //   Serial.println();
+      //   Serial.println("- thanh cong luu den: " + fbdo.dataPath());
+      //   Serial.println(" (" + fbdo.dataType() + ") ");
+      // }
+      // else {
+      //   Serial.println("That bai: " + fbdo.errorReason());
+      // }
+    }
+    prev_send_data = now3;
+  }
 } 
