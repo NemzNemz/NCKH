837314db465f601efdd3bb3acce734e9bab9c172
NemzNemz <urgot0933@gmail.com>
2025-03-15 18:28:44 +0700

    Chu thich chi tiet hon, tang pdMS_TO_TICKS len 500

--- SLAVE1_ORG/SLAVE1_ORG.ino
+++ SLAVE1_ORG/SLAVE1_ORG.ino
@@ -11,15 +11,18 @@
   uint16_t interval_main = 2000;
 
   struct SLAVE_CFG{
-    const uint8_t SLAVE_ID = 1; 
     const char* SLAVE_COMMAND = "SLV_01";  
     const int BAUD_RATE = 115200;
   }slv;
 
+  //Cau hinh UART
   struct uart_variable{
     const uart_port_t uart_num = UART_NUM_1;
+    //Bo dem nhan(rx) 1024 byte
     const int rx_buffer_size = 1024;
+    //Bo dem gui(tx) 1024 byte
     const int tx_buffer_size = 1024;
+    //Hang doi size 10
     const int queue_size = 10;
   }uart_var;
   QueueHandle_t uart_queue;
@@ -33,42 +36,50 @@
     }
     Serial.println("\nWiFi Connected!");
     pin_cfg_init(&pin);
-
+    
+    //Cau hinh UART_1, voi che do 8N1, xai xung mac dinh
     uart_config_t uart_config = {
       .baud_rate = slv.BAUD_RATE,
       .data_bits = UART_DATA_8_BITS,
       .parity = UART_PARITY_DISABLE,
       .stop_bits = UART_STOP_BITS_1,
       .flow_ctrl = UART_HW_FLOWCTRL_DISABLE,
-      .source_clk = UART_SCLK_APB,
+      .source_clk = UART_SCLK_APB
     };
     uart_param_config(uart_var.uart_num, &uart_config);
-
+    //Gan chan UART1 bang TX_RX, khong su dung CTS/RST
     uart_set_pin(uart_var.uart_num, pin.ZIGBEE_TX, pin.ZIGBEE_RX, UART_PIN_NO_CHANGE, UART_PIN_NO_CHANGE);
+    //Cai dat driver ngat UART voi hang doi su kien
     uart_driver_install(uart_var.uart_num, uart_var.rx_buffer_size, uart_var.tx_buffer_size, uart_var.queue_size, &uart_queue, 0);
 
     pinMode(pin.VCC_WTR, OUTPUT);
     pinMode(pin.WTR_PIN, INPUT);
+    //Do phan giai Analog 12bit de su dung TCRT tam thoi
     analogReadResolution(12);
   }
 
   void loop() {
     uart_event_t event;
-    if(xQueueReceive(uart_queue, &event, pdMS_TO_TICKS(300))){
+    //Neu co su kien (wait max 300ms)
+    if(xQueueReceive(uart_queue, &event, pdMS_TO_TICKS(500))){
+      //Neu su kien la UART_DATA
       if(event.type == UART_DATA){
+        //Tao buffer luu data
         char rx_buf[256];
+        //doc tung byte data UART vao bo nho dem(buffer) bang UART_read_bytes
         int len = uart_read_bytes(uart_var.uart_num, rx_buf, event.size, 0);
+        //Mo rong neu nhu co van de ve data, dam bao data khong rong
+        //if(len > 0){}
+        //Dam bao ket thuc chuoi
         rx_buf[len] = '\0';
-
+        
+        //Neu data nhan la SLAVE_COMMAND(SLAVE_1)
         if(strstr(rx_buf, slv.SLAVE_COMMAND) != NULL){
-          // char buffer[40];
-          // snprintf(buffer, sizeof(buffer), "WTR: %d\n", data.water_level);
-          // uart_write_bytes(uart_var.uart_num, buffer, strlen(buffer));
-          // Serial.println("Gui: " + String(buffer));
-
           int tcrtValue = analogRead(TCRT5000_PIN);
           char tcrt_buf[40];
+          //Luu buffer bang snprintf voi sizeof (data cuar tcrt_buf)
           snprintf(tcrt_buf, sizeof(tcrt_buf), "WTR: %d\n", tcrtValue);
+          //Gui qua master bang uart_write_bytes
           uart_write_bytes(uart_var.uart_num, tcrt_buf, strlen(tcrt_buf));
           Serial.println("Gui: " + String(tcrt_buf));
         }
@@ -76,7 +87,6 @@
     } else {
       Serial.println("No data yet");
     }
-
     // unsigned long now_main = millis();
     // if(now_main - prev_main >= interval_main){
     //   raw_wtr(&data);
