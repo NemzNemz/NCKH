8cb4c68f49a77925e6b74a6d75bb3acd7a41eb44
NemzNemz <urgot0933@gmail.com>
2025-04-01 23:02:55 +0700

    fix van de bien toan cuc thanh cong

--- MASTER_ORG/MASTER_ORG.ino
+++ MASTER_ORG/MASTER_ORG.ino
@@ -11,7 +11,7 @@
 #include <WiFi.h>
 
 //Config Firebase
-#define WIFI_SSID "Redmi K60"
+#define WIFI_SSID "Been"
 #define WIFI_PASS "11119999"
 #define API_KEY "AIzaSyCNLfTrT6w3K2ipz9DBT198YocfGbZarII"
 #define DATABASE_URL "https://nckh-dd303-default-rtdb.firebaseio.com/"
@@ -26,7 +26,6 @@ Adafruit_ILI9341 tft = Adafruit_ILI9341(lcd.TFT_CS, lcd.TFT_DC, lcd.TFT_MOSI, lc
 //Poll ID
 unsigned long prev = 0;
 uint16_t interval = 2000;
-static uint8_t slave_id = 1;
 
 //Nhan Data
 unsigned long prev2 = 0;
@@ -99,21 +98,6 @@ void setup() {
     Serial.println(WiFi.localIP());
     Serial.println();
 
-    //Cau hinh UART_1, voi che do 8N1, xai xung mac dinh
-    // uart_config_t uart_config = {
-    //   .baud_rate = 115200,
-    //   .data_bits = UART_DATA_8_BITS,
-    //   .parity = UART_PARITY_DISABLE,
-    //   .stop_bits = UART_STOP_BITS_1,
-    //   .flow_ctrl = UART_HW_FLOWCTRL_DISABLE,
-    //   .source_clk = UART_SCLK_APB
-    // };
-    // uart_param_config(uart_var.uart_num, &uart_config);
-    // //Gan chan UART1 bang TX_RX, khong su dung CTS/RST
-    // uart_set_pin(uart_var.uart_num, pin.ZIGBEE_TX, pin.ZIGBEE_RX, UART_PIN_NO_CHANGE, UART_PIN_NO_CHANGE);
-    // //Cai dat driver ngat UART voi hang doi su kien
-    // uart_driver_install(uart_var.uart_num, uart_var.rx_buffer_size, uart_var.tx_buffer_size, uart_var.queue_size, &uart_queue, 0);
-
     //Ket noi Firebase
     config.api_key = API_KEY;
     config.database_url = DATABASE_URL;
@@ -131,62 +115,38 @@ void setup() {
     peripheral_init(&pin);
     in_text_ra_lcd();
     zigbeeSerial.begin(115200, SERIAL_8N1, pin.ZIGBEE_RX, pin.ZIGBEE_TX);
-    //xTaskCreate(uart_interrupt, "uart_task", 4096, NULL, 5, NULL);
 }
 
-// void uart_interrupt(void *pvParameters){
-//   uart_event_t event;
-//   char rx_buf[256];
-//   while(true){
-//     if(xQueueReceive(uart_queue, (void*)&event, pdMS_TO_TICKS(500))){
-//       if(event.type == UART_DATA){
-//         int len = uart_read_bytes(uart_var.uart_num, rx_buf, event.size, 0);
-//         rx_buf[len] = '\0';
-//         Serial.print("Da nhan data UART!");
-//         Serial.println(rx_buf);
-//       }
-//     }
-//   }
-// }
-
-void xu_ly_data(char* data){
-  //tim chuoi con trong chuoi lon
-  if(strncmp(data, "WTR: ", 5) == 0){
-    char* valStr = data + 5;
+void xu_ly_data(char* data, uint8_t slave_id) {
+  if (strncmp(data, "SLV1: WTR: ", 11) == 0) {
+    char* valStr = data + 11;
     float value = atof(valStr);
-
-    if(slave_id == 1){
-      if(value != lastValueSLV1){
-        tft.setCursor(130, 35); 
-        tft.print(value);
-        lastValueSLV1 = value;
-      }
+    tft.setCursor(130, 35);
+    //Han che x.000
+    tft.print(value, 0);
     }
-    else if(slave_id == 2){
-      if(value != lastValueSLV2){
-        tft.setCursor(130, 55); 
-        tft.print(value);
-        lastValueSLV2 = value;
-      }
-    }
+   else if (strncmp(data, "SLV2: WTR: ", 11) == 0) {
+    char* valStr = data + 11;
+    float value = atof(valStr);
+    tft.setCursor(130, 55);
+    //Han che x.000
+    tft.print(value, 0);
   }
 }
 
-void nhan_data(){
-  if(zigbeeSerial.available()){
-   char c = zigbeeSerial.read();
-    if(c == '\n'){
+void nhan_data(uint8_t slave_id) {
+  if (zigbeeSerial.available()) {
+    char c = zigbeeSerial.read();
+    if (c == '\n') {
       buffer[buf_index] = '\0';
-      //Serial.println("Nhan: " +String(buffer));
       unsigned long now2 = millis();
-      if(now2 - prev2 > interval2){
-      xu_ly_data(buffer);
-      prev2 = now2;
+      if (now2 - prev2 > interval2) {
+        xu_ly_data(buffer, slave_id);
+        prev2 = now2;
       }
       buf_index = 0;
-    }
-    else{
-      if(buf_index < sizeof(buffer) - 1){
+    } else {
+      if (buf_index < sizeof(buffer) - 1) {
         buffer[buf_index++] = c;
       }
     }
@@ -194,21 +154,9 @@ void nhan_data(){
 }
 
 void loop() {
-  // unsigned long now = millis();
-  // if (now - prev > interval) {
-  //   if (slave_id == 1) {
-  //     uart_write_bytes(uart_var.uart_num, "SLV_01\n", 7);
-  //     Serial.println("Gui: SLV_01");
-  //     slave_id = 2;
-  //   } else if (slave_id == 2) {
-  //     uart_write_bytes(uart_var.uart_num, "SLV_02\n", 7);
-  //     Serial.println("Gui: SLV_02");
-  //     slave_id = 1;
-  //   }
-  //   prev = now;
-  // }
+  static uint8_t slave_id = 1;
   poll_id(slave_id);
-  nhan_data();
+  nhan_data(slave_id);
   // unsigned long now3 = millis();
   // if(now3 - prev_send_data > interval3){
   //   if(Firebase.ready() && signUP_OK == true){
