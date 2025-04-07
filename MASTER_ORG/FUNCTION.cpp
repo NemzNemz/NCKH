bca5449c7bc66174f1aace4b6a29bbf075af0962
NemzNemz <urgot0933@gmail.com>
2025-04-07 21:23:47 +0700

    Cap nhat 2 ham lien quan den Firebase

--- MASTER_ORG/FUNCTION.cpp
+++ MASTER_ORG/FUNCTION.cpp
@@ -1,14 +1,39 @@
-  #include "FUNCTION.h"
+#include "FUNCTION.h"
 
-void poll_id(uint8_t &slave_id){
+extern HardwareSerial zigbeeSerial;
+extern FirebaseData fbdo;
+extern bool signUpOK;
+extern int ledStatus;
+extern const int LED_PIN;
+
+void tokenStatusCallback(token_info_t info) {
+    if (info.status == token_status_error) {
+        String errorMessage = String(info.error.message.c_str());
+        Serial.println("Lỗi token: " + errorMessage);
+    }
+}
+
+void readFirebaseData() {
+    if (Firebase.ready() && signUpOK) {
+        if (Firebase.RTDB.getInt(&fbdo, "Sensor/LED")) {
+            ledStatus = atoi(fbdo.stringData().c_str());
+            digitalWrite(LED_PIN, ledStatus);
+            //Serial.println(ledStatus ? "LED BẬT!" : "LED TẮT!");
+        } else {
+            String errorReason = String(fbdo.errorReason().c_str());
+            Serial.println("Lỗi đọc từ Firebase: " + errorReason);
+        }
+    }
+}
+
+void poll_id(uint8_t &slave_id) {
   unsigned long now = millis();
-  if(now - prev > interval){
-    if(slave_id == 1){
+  if(now - prev > interval) {
+    if(slave_id == 1) {
       zigbeeSerial.println("SLV_01");
       Serial.println("Gui: SLV_01");
       slave_id = 2;
-    }
-    else if(slave_id == 2){
+    } else if(slave_id == 2) {
       zigbeeSerial.println("SLV_02");
       Serial.println("Gui: SLV_02");
       slave_id = 1;
