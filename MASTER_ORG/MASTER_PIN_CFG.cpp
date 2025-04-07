b0cec5e74808aff746fe6f3bf41289879378c5aa
NemzNemz <urgot0933@gmail.com>
2025-04-07 21:26:45 +0700

    Xoa bo cau hinh chan khong su dung

--- MASTER_ORG/MASTER_PIN_CFG.cpp
+++ MASTER_ORG/MASTER_PIN_CFG.cpp
@@ -2,14 +2,6 @@
 
 peripheral pin;
 
-// void lcd_init(lcd_pin *lcd){
-//   lcd -> TFT_CS = 0;
-//   lcd -> TFT_DC = 2;
-//   lcd -> TFT_MOSI = 14;
-//   lcd -> TFT_SCLK = 12;
-//   lcd -> TFT_RST = 13;
-// }
-
 void peripheral_init(peripheral *pin){
   pin ->ZIGBEE_RX = 16;
   pin ->ZIGBEE_TX = 17;
