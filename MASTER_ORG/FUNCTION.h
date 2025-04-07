f22b1878b7a7561291cf85a1af0914d920b6e689
NemzNemz <urgot0933@gmail.com>
2025-04-07 21:24:07 +0700

    Them 2 ham prototype ve Firebase

--- MASTER_ORG/FUNCTION.h
+++ MASTER_ORG/FUNCTION.h
@@ -3,12 +3,13 @@
 
 #include <Arduino.h>
 #include <HardwareSerial.h>
+#include <Firebase_ESP_Client.h>
 
 extern unsigned long prev;
 extern uint16_t interval;
-extern HardwareSerial zigbeeSerial;
 
 void poll_id(uint8_t &slave_id);
-void firebase();
+void tokenStatusCallback(token_info_t info);
+void readFirebaseData();
 
 #endif FUNCTION_H
