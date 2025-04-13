74ba27564aa5011ca31e32f34fec374165b0cae9
NemzNemz <urgot0933@gmail.com>
2025-04-14 01:40:04 +0700

    Them ham tinh toan cam bien PH

--- SLAVE1_ORG/Sensor_data.cpp
+++ SLAVE1_ORG/Sensor_data.cpp
@@ -2,9 +2,22 @@
 #include "PIN_CFG.h"
 sensor data;
 
+float cal_ph(uint8_t pin, sensor *data){
+  float adc_raw = analogRead(pin);
+  float voltage = (adc_raw*3.3)/4095.0;
+  
+  const float V7 = 2.50;
+  const float V10 = 2.94;
+  float slope = (V7 - V10) / (7.0 - 10.0);
+
+  float ph = 7.0 +((voltage - V7) / slope);
+  data->ph_value = ph;
+  return data->ph_value;
+}
+
 int raw_wtr(sensor *data){
   static unsigned long prev = 0;
-  const uint16_t interval = 2000;
+  const uint16_t interval = 20;
   unsigned long now = millis();
   if(now - prev >= interval){
     digitalWrite(pin.VCC_WTR, HIGH);
