e4baa40151174993b7a8129a3c12815402162ec8
NemzNemz <urgot0933@gmail.com>
2025-04-14 01:40:22 +0700

    Them prototype ham tinh PH

--- SLAVE1_ORG/Sensor_data.h
+++ SLAVE1_ORG/Sensor_data.h
@@ -6,6 +6,7 @@
 typedef struct Sensor_Data{
   float water_level;
   int raw_value_wtr;
+  float ph_value;
   //Se con nhieu cam bien hon nua (3~5)
 } sensor;
 
@@ -14,5 +15,6 @@ extern sensor data;
 
 int raw_wtr(sensor *data);
 float water_lv(sensor *data);
+float cal_ph(uint8_t pin, sensor *data);
 
 #endif
