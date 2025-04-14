5e27a2736f5d2ca416ee11cd522018a4ba789655
NemzNemz <urgot0933@gmail.com>
2025-04-15 00:03:53 +0700

    Cap nhat them ham nhan data de thuc hien Daily Task, toi uu nhe ma nguon

--- MASTER_ORG/FUNCTION.cpp
+++ MASTER_ORG/FUNCTION.cpp
@@ -5,6 +5,11 @@ extern FirebaseData fbdo;
 extern bool signUpOK;
 extern int ledStatus;
 
+extern int taskHour_on;    
+extern int taskMinute_on;    
+extern int taskHour_off;    
+extern int taskMinute_off;  
+
 void tokenStatusCallback(token_info_t info) {
     if (info.status == token_status_error) {
         String errorMessage = String(info.error.message.c_str());
@@ -23,6 +28,34 @@ void readFirebaseData() {
         }
     }
 }
+
+void readDailyTaskSchedule() {
+  if (Firebase.RTDB.getString(&fbdo, "Daily_Task/Hour_on")) {
+    taskHour_on = atoi(fbdo.stringData().c_str());
+    Serial.print("Hour_on: ");
+    Serial.println(taskHour_on);
+  }
+
+  if (Firebase.RTDB.getString(&fbdo, "Daily_Task/Minutes_on")) {
+    taskMinute_on = atoi(fbdo.stringData().c_str());
+    Serial.print("Minutes_on: ");
+    Serial.println(taskMinute_on);
+  }
+
+  if (Firebase.RTDB.getString(&fbdo, "Daily_Task/Hour_off")) {
+    taskHour_off = atoi(fbdo.stringData().c_str());
+    Serial.print("Hour_off: ");
+    Serial.println(taskHour_off);
+  }
+
+  if (Firebase.RTDB.getString(&fbdo, "Daily_Task/Minutes_off")) {
+    taskMinute_off = atoi(fbdo.stringData().c_str());
+    Serial.print("Minutes_off: ");
+    Serial.println(taskMinute_off);
+  }
+}
+
+
 
 void poll_id(uint8_t &slave_id) {
   unsigned long now = millis();
