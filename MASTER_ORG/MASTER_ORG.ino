dcacc734d1d62374330fd717f79251620ed8c26b
NemzNemz <urgot0933@gmail.com>
2025-04-20 00:39:31 +0700

    Sua co che nhan tin nhan, giam tai viec log hang loat

--- MASTER_ORG/MASTER_ORG.ino
+++ MASTER_ORG/MASTER_ORG.ino
@@ -5,10 +5,17 @@
 #include <WiFi.h>
 
 //Config Firebase
-#define WIFI_SSID "Been"
-#define WIFI_PASS "11119999"
+#define WIFI_SSID "Trieu Ninh"
+#define WIFI_PASS "12344321"
 #define API_KEY "AIzaSyCNLfTrT6w3K2ipz9DBT198YocfGbZarII"
 #define DATABASE_URL "https://nckh-dd303-default-rtdb.firebaseio.com/"
+
+bool    frameOn  = false;     // đang trong giai đoạn ghi frame
+size_t  frameIdx = 0;         // chỉ số write buffer
+char    frameBuf[64];         // lưu khung "<Sx…>\n"
+
+char buffer[40];
+int  buf_index = 0;
 
 //Noi luu gio, phut chay daily_task
 int taskHour_on = 0;    
@@ -39,10 +46,15 @@ int last_run_day_on = -1;
 int last_run_day_off = -1;
 
 //Bien kiem soat data
-float last_WTR_SLV1 = 0.0;          
-float last_WTR_SLV2 = 0.0; 
-float last_PH_SLV1 = 0.0;
-float last_PH_SLV2 = 0.0;
+typedef struct last_data_value{
+  float last_WTR_SLV1;          
+  float last_WTR_SLV2; 
+  float last_PH_SLV1;
+  float last_PH_SLV2;
+  float last_TDS_SLV1;
+  float last_TDS_SLV2 ;
+}last_data;
+last_data data = {0.0, 0.0, 0.0, 0.0, 0.0, 0.0};
 
 //Khoi tao LCD
 lcd_pin lcd;
@@ -52,20 +64,18 @@ Adafruit_ILI9341 tft = Adafruit_ILI9341(lcd.TFT_CS, lcd.TFT_DC, lcd.TFT_MOSI, lc
 unsigned long prev = 0;
 uint16_t interval = 5000;
 
-//Nhan Data
-unsigned long prev_receive = 0;
-uint16_t interval_receive = 1700;
-
-//Gui Data         
-unsigned long prev_send_data = 0;   
-const uint16_t interval_send_data = 5000;    
-
-//Debounce nut nhan
-volatile unsigned long prev_debounce = 0;
-const uint8_t interval_debounce = 130;
-
-char buffer[40];
-int buf_index = 0;
+typedef struct timing_variables{
+  //Gui len firebase
+  unsigned long prev_send_fb;   
+  uint16_t interval_send_fb;    
+  //Gui Data         
+  unsigned long prev_send_data;   
+  uint16_t interval_send_data;    
+  //Debounce nut nhan
+  volatile unsigned long prev_debounce;
+  uint8_t interval_debounce;
+}timing_var;
+timing_var timing ={0, 1000, 0, 5000, 0, 150};
 
 //Chong xung dot
 portMUX_TYPE mux = portMUX_INITIALIZER_UNLOCKED;
@@ -75,11 +85,11 @@ void IRAM_ATTR nhan_nut(){
   unsigned long now_debounce = millis();
   portENTER_CRITICAL_ISR(&mux);
 
-  if(now_debounce - prev_debounce > interval_debounce){
+  if(now_debounce - timing.prev_debounce > timing.interval_debounce){
     //Serial.println("Da an nut!");
     ledStatus = !ledStatus;
     digitalWrite(pin.LED_PIN, ledStatus);
-    prev_debounce = now_debounce;
+    timing.prev_debounce = now_debounce;
   }
   portEXIT_CRITICAL_ISR(&mux);
 }
@@ -98,18 +108,22 @@ void in_text_ra_lcd(){
     tft.setCursor(20, 40);
     tft.println("SLV1_DATA");
     tft.setCursor(20, 70);
-    tft.println("P.H:");
+    tft.println("TDS:");
     tft.setCursor(20, 100);
+    tft.println("P.H:");
+    tft.setCursor(20, 130);
     tft.println("WTR:");
     
     tft.setCursor(200, 40);
     tft.println("SLV2_DATA");
     tft.setCursor(200, 70);
-    tft.println("P.H:");
+    tft.println("TDS:");
     tft.setCursor(200, 100);
+    tft.println("P.H:");
+    tft.setCursor(200, 130);
     tft.println("WTR:");
 
-    tft.setCursor(20, 150);
+    tft.setCursor(20, 160);
     tft.setTextColor(ILI9341_RED, ILI9341_BLACK);
     tft.println("LED_STATE:");
 }
@@ -144,115 +158,149 @@ void daily_task_test_off(){
 }
 
 void setup() {
-    Serial.begin(115200);
-    peripheral_init(&pin);
-    pinMode(pin.LED_PIN, OUTPUT);
-    pinMode(pin.BUTTON_PIN, INPUT_PULLUP);
-    attachInterrupt(digitalPinToInterrupt(pin.BUTTON_PIN), nhan_nut, FALLING);
-    digitalWrite(pin.LED_PIN, 0);
+  Serial.begin(115200);
+  peripheral_init(&pin);
+  pinMode(pin.LED_PIN, OUTPUT);
+  pinMode(pin.BUTTON_PIN, INPUT_PULLUP);
+  attachInterrupt(digitalPinToInterrupt(pin.BUTTON_PIN), nhan_nut, FALLING);
+  digitalWrite(pin.LED_PIN, 0);
 
-    WiFi.begin(WIFI_SSID, WIFI_PASS);
-    Serial.println("Dang ket noi voi Wifi");
-    while(WiFi.status() != WL_CONNECTED){
-      Serial.println(".");
-      delay(400);
-    }
-    Serial.println("DA KET NOI");
-    Serial.println(WiFi.localIP());
-    Serial.println();
-    //Lay thoi gian
-    sync_time();
+  WiFi.begin(WIFI_SSID, WIFI_PASS);
+  Serial.println("Dang ket noi voi Wifi");
+  while(WiFi.status() != WL_CONNECTED){
+    Serial.println(".");
+    delay(400);
+  }
+  Serial.println("DA KET NOI");
+  Serial.println(WiFi.localIP());
+  Serial.println();
+  //Lay thoi gian
+  sync_time();
 
-    //Ket noi Firebase
-    config.api_key = API_KEY;
-    config.database_url = DATABASE_URL;
-    if(Firebase.signUp(&config, &auth, "", "")){
-      Serial.println("Sign UP!");
-      signUpOK = true;
-    }
-    else{
-      Serial.printf("%s\n", config.signer.signupError.message.c_str());
-    }
-    config.token_status_callback = tokenStatusCallback;
-    Firebase.begin(&config, &auth);
-    Firebase.reconnectWiFi(true);
-    //Doc cau hinh thoi gian
-    readDailyTaskSchedule();  
+  //Ket noi Firebase
+  config.api_key = API_KEY;
+  config.database_url = DATABASE_URL;
+  if(Firebase.signUp(&config, &auth, "", "")){
+    Serial.println("Sign UP!");
+    signUpOK = true;
+  }
+  else{
+    Serial.printf("%s\n", config.signer.signupError.message.c_str());
+  }
+  config.token_status_callback = tokenStatusCallback;
+  Firebase.begin(&config, &auth);
+  Firebase.reconnectWiFi(true);
+  //Doc cau hinh thoi gian
+  //readDailyTaskSchedule();  
 
-    in_text_ra_lcd();
-    zigbeeSerial.begin(115200, SERIAL_8N1, pin.ZIGBEE_RX, pin.ZIGBEE_TX);
+  in_text_ra_lcd();
+  zigbeeSerial.begin(115200, SERIAL_8N1, pin.ZIGBEE_RX, pin.ZIGBEE_TX);
 }
 
-void xu_ly_data(char* data, uint8_t slave_id) {
-  if (strncmp(data, "SLV1: P.H: ", 11) == 0) {
-    char* valStr = data + 11;
-    float value = atof(valStr);
-    last_PH_SLV1 = value;
-    //Xoa vung hien thi cu, set mau xanh la
-    tft.setTextColor(ILI9341_GREEN, ILI9341_BLACK);
-    tft.fillRect(80, 70, 60, 15, ILI9341_RED);
-    //delay(1000);
-    tft.setCursor(80, 70);
-    //Han che x.000
-    tft.print(value, 1);
-  }
-
-  else if (strncmp(data, "SLV2: WTR: ", 11) == 0) {
-    char* valStr = data + 11;
-    float value = atof(valStr);
-    last_WTR_SLV2 = value;
+void processSLV2Data(const char *s) {
+  float wtr, ph, tds;
+  if (sscanf(s, "<S2,WTR=%f,PH=%f,TDS=%f>", &wtr, &ph, &tds) == 3) {
+    data.last_WTR_SLV2 = wtr;
+    data.last_PH_SLV2  = ph;
+    data.last_TDS_SLV2 = tds;
 
     tft.setTextColor(ILI9341_GREEN, ILI9341_BLACK);
-    tft.fillRect(260, 70, 60, 15, ILI9341_RED);
-    tft.setCursor(260, 70);
-    //Han che x.000
-    tft.print(value, 1);
-  }
+    //Ve TDS, PH va WTR
+    tft.fillRect(260, 70, 60, 15, ILI9341_BLACK);
+    tft.setCursor(260, 70);  
+    tft.print(tds, 1);
 
-  else if(strncmp(data, "SLV1: WTR: ", 11) == 0){
-    char* valStr = data + 11;
-    float value = atof(valStr);
-    last_WTR_SLV1 = value;
+    tft.fillRect(260, 100, 60, 15, ILI9341_BLACK);
+    tft.setCursor(260, 100);  
+    tft.print(ph, 1);
 
-    tft.setTextColor(ILI9341_GREEN, ILI9341_BLACK);
-    tft.fillRect(80, 100, 60, 15, ILI9341_RED);
-    //delay(1000);
-    tft.setCursor(80, 100);
-    //Han che x.000
-    tft.print(value, 1);
+    tft.fillRect(260, 130, 60, 15, ILI9341_BLACK);
+    tft.setCursor(260, 130);  
+    tft.print(wtr, 1);
   }
 }
 
-void nhan_data(uint8_t slave_id) {
-  if (zigbeeSerial.available()) {
+void processSLV1Data(const char *s) {
+  //Tham so nhan vao la 1 chuoi
+  float wtr, ph, tds;
+  // sscanf bo qua '<', 'S1,' chi doc WTR, PH, TDS
+  if (sscanf(s, "<S1,WTR=%f,PH=%f,TDS=%f>", &wtr, &ph, &tds) == 3) {
+    data.last_WTR_SLV1 = wtr;
+    data.last_PH_SLV1  = ph;
+    data.last_TDS_SLV1 = tds;
+
+    tft.setTextColor(ILI9341_GREEN, ILI9341_BLACK);
+    //Ve TDS, PH va WTR
+    tft.fillRect( 80, 70, 60, 15, ILI9341_BLACK);
+    tft.setCursor(80, 70);  
+    tft.print(tds, 1);
+
+    tft.fillRect( 80, 100, 60, 15, ILI9341_BLACK);
+    tft.setCursor(80,100);  
+    tft.print(ph,  1);
+
+    tft.fillRect( 80, 130, 60, 15, ILI9341_BLACK);
+    tft.setCursor(80,130);  
+    tft.print(wtr, 1);
+  }
+}
+
+void nhan_data() {
+  while (zigbeeSerial.available()) {
     char c = zigbeeSerial.read();
-    if (c == '\n') {
-      buffer[buf_index] = '\0';
-      
-      // Serial.print("Chuoi nhan duoc: ");
-      // Serial.println(buffer);
 
-      unsigned long now_receive = millis();
-      if (now_receive - prev_receive > interval_receive) {
-        xu_ly_data(buffer, slave_id);
-        prev_receive = now_receive;
+    // Bỏ CR luôn 
+    if (c == '\r') continue;
+
+    //Nếu chưa bắt đầu frame, chỉ quan tâm đến '<' 
+    if (!frameOn) {
+      if (c == '<') {
+        frameOn = true;
+        frameIdx = 0;
+        frameBuf[frameIdx++] = c;   // ghi '<'
       }
-      buf_index = 0;
-    } else {
-      if (buf_index < sizeof(buffer) - 1) {
-        buffer[buf_index++] = c;
-      }
+      continue;  // bỏ qua mọi byte khác
+    }
+
+    //Đang trong frame: ghi tiếp nếu đủ chỗ
+    if (frameIdx < sizeof(frameBuf) - 1) {
+      frameBuf[frameIdx++] = c;
+    } 
+    else {
+    //overflow: hủy khung
+      frameOn  = false;
+      frameIdx = 0;
+      continue;
+    }
+
+    // Khi gặp newline, kết thúc frame
+    if(c == '\n') {
+      frameBuf[frameIdx] = '\0';   // đóng chuỗi
+      Serial.print("Chuoi nhan duoc: ");
+      Serial.println(frameBuf);
+
+    // GỌI HÀM XỬ LÝ
+    if(strncmp(frameBuf, "<S1,", 4) == 0) processSLV1Data(frameBuf);
+    else if(strncmp(frameBuf, "<S2,", 4) == 0) processSLV2Data(frameBuf);
+    else Serial.println("Khong nhan dien duoc slave_id");
+
+    // Reset để đón frame kế
+    frameOn  = false;
+    frameIdx = 0;
     }
   }
 }
 
 void send_value_to_firebase(){
   unsigned long now_send_data = millis();
-    if (now_send_data - prev_send_data > interval_send_data) {
+    if (now_send_data - timing.prev_send_data > timing.interval_send_data) {
       if (Firebase.ready() && signUpOK) {
-        float WTR_1 = last_WTR_SLV1;
-        float WTR_2 = last_WTR_SLV2;
-        float PH_1 = last_PH_SLV1;
+        float WTR_1 = data.last_WTR_SLV1;
+        float WTR_2 = data.last_WTR_SLV2;
+        float PH_1 = data.last_PH_SLV1;
+        float PH_2 = data.last_PH_SLV2;
+        float TDS_1 = data.last_TDS_SLV1;
+        float TDS_2 = data.last_TDS_SLV2;
           //Gui WTR_SLV1
           if (Firebase.RTDB.setFloat(&fbdo, "Sensor/WTR_data1", WTR_1)) {
             //Serial.println();
@@ -262,15 +310,7 @@ void send_value_to_firebase(){
           else{
             Serial.println("Thất bại: " + fbdo.errorReason());
           }
-          //Gui WTR_SLV2
-          if(Firebase.RTDB.setFloat(&fbdo, "Sensor/WTR_data2", WTR_2)) {
-            //Serial.println();
-            //Serial.println("- Thành công lưu đến: " + fbdo.dataPath());
-            //Serial.println(" (" + fbdo.dataType() + ") ");
-          }
-          else {
-            Serial.println("Thất bại: " + fbdo.errorReason());
-          }
+
           //Gui PH_SLV1
           if(Firebase.RTDB.setFloat(&fbdo, "Sensor/PH_data1", PH_1)) {
             //Serial.println();
@@ -280,8 +320,48 @@ void send_value_to_firebase(){
           else {
             Serial.println("Thất bại: " + fbdo.errorReason());
           }
+
+          //Gui TDS_SLV1
+          if(Firebase.RTDB.setFloat(&fbdo, "Sensor/TDS_data1", TDS_1)) {
+            //Serial.println();
+            //Serial.println("- Thành công lưu đến: " + fbdo.dataPath());
+            //Serial.println(" (" + fbdo.dataType() + ") ");
+          }
+          else {
+            Serial.println("Thất bại: " + fbdo.errorReason());
+          }
+
+          //Gui WTR_SLV2
+          if(Firebase.RTDB.setFloat(&fbdo, "Sensor/WTR_data2", WTR_2)) {
+            //Serial.println();
+            //Serial.println("- Thành công lưu đến: " + fbdo.dataPath());
+            //Serial.println(" (" + fbdo.dataType() + ") ");
+          }
+          else {
+            Serial.println("Thất bại: " + fbdo.errorReason());
+          }
+
+          //Gui PH_SLV2
+          if(Firebase.RTDB.setFloat(&fbdo, "Sensor/PH_data2", PH_2)) {
+            //Serial.println();
+            //Serial.println("- Thành công lưu đến: " + fbdo.dataPath());
+            //Serial.println(" (" + fbdo.dataType() + ") ");
+          }
+          else {
+            Serial.println("Thất bại: " + fbdo.errorReason());
+          }
+
+          //Gui TDS_SLV2
+          if(Firebase.RTDB.setFloat(&fbdo, "Sensor/TDS_data2", TDS_2)) {
+            //Serial.println();
+            //Serial.println("- Thành công lưu đến: " + fbdo.dataPath());
+            //Serial.println(" (" + fbdo.dataType() + ") ");
+          }
+          else {
+            Serial.println("Thất bại: " + fbdo.errorReason());
         }
-    prev_send_data = now_send_data;
+      timing.prev_send_data = now_send_data;
+    }
   }
 }
 
@@ -301,8 +381,8 @@ void lcd_change_led_state(){
   if (ledStatus != oldLedStatus) {
   //Chi cap nhat LCD khi co thay doi de do lagg
     tft.setTextColor(ILI9341_RED, ILI9341_BLACK);
-    tft.fillRect(160, 150, 60, 15, ILI9341_BLACK);
-    tft.setCursor(150, 150);
+    tft.fillRect(160, 160, 60, 15, ILI9341_BLACK);
+    tft.setCursor(150, 160);
     if (ledStatus == 1) {
       tft.print("ON");
     } 
@@ -315,18 +395,24 @@ void lcd_change_led_state(){
 
 void loop() {
   static uint8_t slave_id = 1;
-  // poll_id(slave_id);
-  // nhan_data(slave_id);
-  // readFirebaseData();
-  // send_value_to_firebase();
+  poll_id(slave_id);
+  nhan_data();
+  //readFirebaseData();
+  //send_value_to_firebase();
+
+  unsigned long nowww = millis();
+  if(nowww - timing.prev_send_fb > timing.interval_send_fb){
+    readFirebaseData();
+    timing.prev_send_fb = nowww;
+  }
 
   //Chi cap nhat Firebase khi co thay doi de do lagg
   if(ledStatus != old_firebase_status){
     send_state_to_firebase();
     lcd_change_led_state();
   }
-  delay(3000);
-  readDailyTaskSchedule();
+  //delay(3000);
+  //readDailyTaskSchedule();
   // Serial.printf("Giờ hiện tại: %02d:%02d:%02d - Ngày: %04d-%02d-%02d\n",
   //               timeinfo.tm_hour, timeinfo.tm_min, timeinfo.tm_sec,
   //               timeinfo.tm_year + 1900, timeinfo.tm_mon + 1, timeinfo.tm_mday);
