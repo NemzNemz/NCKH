18574be09b271fddc63f932ab8a9d4f3e14482fd
NemzNemz <urgot0933@gmail.com>
2025-04-20 00:43:06 +0700

    Thay doi 1 chut cau hinh chan

--- SLAVE2_OORG/PIN_CFG.cpp
+++ SLAVE2_OORG/PIN_CFG.cpp
@@ -6,5 +6,7 @@ void pin_cfg_init(peripheral *pin){
   pin->ZIGBEE_RX = 16; //UART
   pin->ZIGBEE_TX = 17;
   pin->VCC_WTR = 12;
-  pin->WTR_PIN = 14;
+  pin->WTR_PIN = 34;
+  pin->P0_PIN = 35;
+  pin->TDS_PIN = 32;
 }
