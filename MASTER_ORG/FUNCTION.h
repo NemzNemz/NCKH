7f19d19496ec640a23ab61bc133a470a40f4db8b
NemzNemz <urgot0933@gmail.com>
2025-04-20 00:39:30 +0700

    Chinh sua ham poll ID

--- MASTER_ORG/FUNCTION.h
+++ MASTER_ORG/FUNCTION.h
@@ -9,6 +9,8 @@
 extern unsigned long prev;
 extern uint16_t interval;
 extern peripheral pin;
+extern char buffer[];
+extern int  buf_index;
 
 void poll_id(uint8_t &slave_id);
 void tokenStatusCallback(token_info_t info);
