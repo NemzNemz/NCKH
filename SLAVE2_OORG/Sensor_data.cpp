478b2a3cb2f7bf5b712524657572efa734d254a3
NemzNemz <urgot0933@gmail.com>
2025-04-20 00:43:08 +0700

    Them ham doc TDS

--- SLAVE2_OORG/Sensor_data.cpp
+++ SLAVE2_OORG/Sensor_data.cpp
@@ -1,10 +1,45 @@
 #include "Sensor_data.h"
 #include "PIN_CFG.h"
 sensor data;
+const uint8_t samples = 30;
+float water_temp = 25.5;
+
+float cal_ph(uint8_t pin, sensor *data){
+  float adc_raw = analogRead(pin);
+  float voltage = (adc_raw*3.3)/4095.0;
+  
+  const float V7 = 2.50;
+  const float V10 = 2.94;
+  float slope = (V7 - V10) / (7.0 - 10.0);
+
+  float ph = 7.0 +((voltage - V7) / slope);
+  data->ph_value = ph;
+  return data->ph_value;
+}
+
+float trung_binh_tds(uint8_t pin){
+  float raw_tds = 0;
+  for(int i = 0; i < samples; i++){
+    raw_tds += analogRead(pin);
+    delay(2);
+  }
+  return raw_tds/ (float)samples;
+}
+
+float tds_calculate(sensor *data, uint8_t pin){
+  float adc_avg = trung_binh_tds(pin);
+  float voltage = adc_avg *3.3/ 4095.0;
+  float ec_value = 133.42 * voltage * voltage * voltage
+                  - 255.86 * voltage * voltage
+                  + 857.39 * voltage;
+  float tds_raw = ec_value * 0.5;
+  float bu_nhiet = 1.0 + 0.02 * (water_temp - 25.0);
+  return data->real_tds_value = tds_raw/ bu_nhiet;
+}
 
 int raw_wtr(sensor *data){
   static unsigned long prev = 0;
-  const uint16_t interval = 2000;
+  const uint16_t interval = 20;
   unsigned long now = millis();
   if(now - prev >= interval){
     digitalWrite(pin.VCC_WTR, HIGH);
