853f8ddc1f665b94feeb8593e68acb2b35f93a19
NemzNemz <urgot0933@gmail.com>
2025-04-23 22:40:16 +0700

    Them prototype ham tinh nhiet do nuoc

--- SLAVE1_ORG/Sensor_data.h
+++ SLAVE1_ORG/Sensor_data.h
@@ -1,24 +1,28 @@
 #ifndef SENSOR_DATA_H
 #define SENSOR_DATA_H
-
+#include <OneWire.h>
+#include <DallasTemperature.h>
 #include <Arduino.h>
 
-typedef struct Sensor_Data{
+typedef struct {
   float water_level;
   int raw_value_wtr;
   float ph_value;
   float real_tds_value;
-  //Se con nhieu cam bien hon nua (3~5)
+  float water_temp;
 } sensor;
-
-//bien toan cuc nhieu file
 extern sensor data;
-extern float water_temp;
+
+extern OneWire* oneWire;
+extern DallasTemperature* sensors;
+
+void init_sensors();
 
 float trung_binh_tds(uint8_t pin);
-float tds_calculate (sensor *data, uint8_t pin);
+float tds_calculate(sensor *data, uint8_t pin);
 int raw_wtr(sensor *data);
 float water_lv(sensor *data);
+float water_temp(sensor *data);
 float cal_ph(uint8_t pin, sensor *data);
 
 #endif
