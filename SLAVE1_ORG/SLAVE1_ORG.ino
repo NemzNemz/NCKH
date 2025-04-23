198adc5668eea2c4ae5f0ca6ed15aeb6c4b4be1f
NemzNemz <urgot0933@gmail.com>
2025-04-23 22:39:24 +0700

    Test log ds18b20 thanh cong

--- SLAVE1_ORG/SLAVE1_ORG.ino
+++ SLAVE1_ORG/SLAVE1_ORG.ino
@@ -4,6 +4,8 @@
 #include "GG_SHEET.h"
 #include "driver/uart.h"
 #include "freertos/queue.h"
+#include <OneWire.h>
+#include <DallasTemperature.h>
 
 struct SLAVE_CFG{
   const char* SLAVE_COMMAND = "SLV_01";  
@@ -31,6 +33,7 @@ void setup() {
   }
   Serial.println("\nWiFi Connected!");
   pin_cfg_init(&pin);
+  init_sensors();
     
   //Cau hinh UART_1, voi che do 8N1, xai xung mac dinh
   uart_config_t uart_config = {
@@ -58,72 +61,65 @@ void setup() {
 void test_in_log(){
   raw_wtr(&data);
   water_lv(&data);
+  water_temp(&data);
   Serial.println("Water Level: " + String(data.water_level));
   cal_ph(pin.P0_PIN, &data);
   Serial.println("PH Level: " + String(data.ph_value));
   trung_binh_tds(pin.TDS_PIN);
   tds_calculate(&data, pin.TDS_PIN);
   Serial.println("Gia tri TDS la: " + String(data.real_tds_value));
+  Serial.println("Nhiet do la: " + String(data.water_temp));
 }
 
 void loop() {
-  // test_in_log();
-  // delay(2000);
-  uart_event_t event;
-  //Neu co su kien (wait max 300ms)
-  if(xQueueReceive(uart_queue, &event, pdMS_TO_TICKS(500))){
-    //Neu su kien la UART_DATA
-    if(event.type == UART_DATA){
-      //Tao buffer luu data
-      char rx_buf[256];
-      //doc tung byte data UART vao bo nho dem(buffer) bang UART_read_bytes
-      int len = uart_read_bytes(uart_var.uart_num, rx_buf, event.size, 0);
-      //Mo rong neu nhu co van de ve data, dam bao data khong rong
-      if(len == 0){
-        uart_flush_input(uart_var.uart_num); 
-        return;
-      }
-      //Dam bao ket thuc chuoi
-      rx_buf[len] = '\0';
+  test_in_log();
+  delay(2000);
+  // uart_event_t event;
+  // //Neu co su kien (wait max 300ms)
+  // if(xQueueReceive(uart_queue, &event, pdMS_TO_TICKS(500))){
+  //   //Neu su kien la UART_DATA
+  //   if(event.type == UART_DATA){
+  //     //Tao buffer luu data
+  //     char rx_buf[50];
+  //     //doc tung byte data UART vao bo nho dem(buffer) bang UART_read_bytes
+  //     int len = uart_read_bytes(uart_var.uart_num, rx_buf, event.size, 0);
+  //     //Mo rong neu nhu co van de ve data, dam bao data khong rong
+  //     if(len == 0){
+  //       uart_flush_input(uart_var.uart_num); 
+  //       return;
+  //     }
+  //     //Dam bao ket thuc chuoi
+  //     rx_buf[len] = '\0';
         
-      //Neu data nhan la SLAVE_COMMAND(SLAVE_1)
-      if((len == 7 || len == 8) && strncmp(rx_buf, "SLV_01", 6) == 0) {
-        raw_wtr(&data);
-        water_lv(&data);
-        cal_ph(pin.P0_PIN, &data);
-        trung_binh_tds(pin.TDS_PIN);
-        tds_calculate(&data, pin.TDS_PIN);
+  //     //Neu data nhan la SLAVE_COMMAND(SLAVE_1)
+  //     if((len == 7 || len == 8) && strncmp(rx_buf, "SLV_01", 6) == 0) {
+  //       raw_wtr(&data);
+  //       water_lv(&data);
+  //       cal_ph(pin.P0_PIN, &data);
+  //       trung_binh_tds(pin.TDS_PIN);
+  //       tds_calculate(&data, pin.TDS_PIN);
 
-        //Frame data, co kich thuoc ~ do dai chuoi
-        char big_frame[70];
-        //Luu buffer bang snprintf voi sizeof (data cuar tcrt_buf)
-        int large_of_frame = snprintf(big_frame, sizeof(big_frame), 
-                                    "<S1,WTR=%.1f,PH=%.1f,TDS=%.0f>\n", 
-                                    data.water_level, data.ph_value, data.real_tds_value);
+  //       //Frame data, co kich thuoc ~ do dai chuoi
+  //       char big_frame[70];
+  //       //Luu buffer bang snprintf voi sizeof (data cuar tcrt_buf)
+  //       uint8_t large_of_frame = snprintf(big_frame, sizeof(big_frame), 
+  //                                   "<S1,WTR=%.1f,PH=%.1f,TDS=%.0f>\n", 
+  //                                   data.water_level, data.ph_value, data.real_tds_value);
         
-        //Kiem tra tran bo dem
-        if(large_of_frame <= 0 || large_of_frame >= sizeof(big_frame)){
-          //neu loi, bo goi tin
-          return;
-        }
-        //Gui qua master bang uart_write_bytes
-        uart_write_bytes(uart_var.uart_num, big_frame, large_of_frame);
-        uart_wait_tx_done(uart_var.uart_num, pdMS_TO_TICKS(20));
-        uart_flush_input(uart_var.uart_num);
+  //       //Kiem tra tran bo dem
+  //       if(large_of_frame <= 0 || large_of_frame >= sizeof(big_frame)){
+  //         //neu loi, bo goi tin
+  //         return;
+  //       }
+  //       //Gui qua master bang uart_write_bytes
+  //       uart_write_bytes(uart_var.uart_num, big_frame, large_of_frame);
+  //       uart_wait_tx_done(uart_var.uart_num, pdMS_TO_TICKS(20));
+  //       uart_flush_input(uart_var.uart_num);
 
-        // In log
-        Serial.print("TX-> ");
-        Serial.write(big_frame, large_of_frame);
-        //delay(1);
-        // uart_write_bytes(uart_var.uart_num, res_buf2, strlen(res_buf2));
-        // delay(1);
-        // uart_write_bytes(uart_var.uart_num, res_buf3, strlen(res_buf3));
-        // delay(1);
-
-        // Serial.print(String(big_frame));
-        // Serial.println(String(res_buf2));
-        // Serial.println(String(res_buf3));
-      }
-    }
-  } 
+  //       // In log
+  //       Serial.print("TX-> ");
+  //       Serial.write(big_frame, large_of_frame);
+  //     }
+  //   }
+  // } 
 }
