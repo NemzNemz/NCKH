52a1c68f1d015c4583e551a804c721716b3409bf
NemzNemz <urgot0933@gmail.com>
2025-04-23 22:37:37 +0700

    Cap nhat FUNCTION.cpp - cau truc lai cac ham firebase, giam extern, them tham so vao ham

--- MASTER_ORG/FUNCTION.cpp
+++ MASTER_ORG/FUNCTION.cpp
@@ -2,13 +2,55 @@
 
 extern HardwareSerial zigbeeSerial;
 extern FirebaseData fbdo;
+extern FirebaseAuth auth;
+extern FirebaseConfig config;
 extern bool signUpOK;
-extern int ledStatus;
 
-extern int taskHour_on;    
-extern int taskMinute_on;    
-extern int taskHour_off;    
-extern int taskMinute_off;  
+void reconnect_firebase() {
+    if (!Firebase.ready() && signUpOK) {
+        Serial.println("Thử kết nối lại Firebase...");
+        Firebase.begin(&config, &auth);
+        Firebase.reconnectWiFi(true);
+    }
+}
+
+bool send_firebase_float(FirebaseData *fbdo, const char *path, float value) {
+    if (Firebase.RTDB.setFloat(fbdo, path, value)) {
+        return true;
+    } else {
+        Serial.println("Thất bại khi gửi " + String(path) + ": " + fbdo->errorReason());
+        return false;
+    }
+}
+
+void send_value_to_firebase(timing_variables *timing, last_data_value *data) {
+    unsigned long now_send_data = millis();
+    if (now_send_data - timing->prev_send_data > timing->interval_send_data) {
+        if (Firebase.ready() && signUpOK) {
+            send_firebase_float(&fbdo, "Sensor/WTR_data1", data->last_WTR_SLV1);
+            send_firebase_float(&fbdo, "Sensor/PH_data1", data->last_PH_SLV1);
+            send_firebase_float(&fbdo, "Sensor/TDS_data1", data->last_TDS_SLV1);
+            send_firebase_float(&fbdo, "Sensor/WTR_data2", data->last_WTR_SLV2);
+            send_firebase_float(&fbdo, "Sensor/PH_data2", data->last_PH_SLV2);
+            send_firebase_float(&fbdo, "Sensor/TDS_data2", data->last_TDS_SLV2);
+            timing->prev_send_data = now_send_data;
+        }
+        if (!Firebase.ready() && signUpOK) {
+            reconnect_firebase();
+        }
+    }
+}
+
+void send_state_to_firebase(status_var *status) {
+    if (Firebase.ready() && signUpOK) {
+        if (Firebase.RTDB.setInt(&fbdo, "Sensor/LED", status->ledStatus)) {
+            Serial.println("Gui trang thai len Firebase thanh cong");
+            status->old_firebase_status = status->ledStatus;
+        } else {
+            Serial.println("Gui firebase that bai: " + String(fbdo.errorReason().c_str()));
+        }
+    }
+}
 
 void tokenStatusCallback(token_info_t info) {
     if (info.status == token_status_error) {
@@ -17,11 +59,11 @@ void tokenStatusCallback(token_info_t info) {
     }
 }
 
-void readFirebaseData() {
+void readFirebaseData(peripheral *pin, status_var *status) {
     if (Firebase.ready() && signUpOK) {
         if (Firebase.RTDB.getInt(&fbdo, "Sensor/LED")) {
-            ledStatus = atoi(fbdo.stringData().c_str());
-            digitalWrite(pin.LED_PIN, ledStatus);
+            status->ledStatus = atoi(fbdo.stringData().c_str());
+            digitalWrite(pin->LED_PIN, status->ledStatus);
         } else {
             String errorReason = String(fbdo.errorReason().c_str());
             Serial.println("Lỗi đọc từ Firebase: " + errorReason);
@@ -29,48 +71,50 @@ void readFirebaseData() {
     }
 }
 
-void readDailyTaskSchedule() {
+void readDailyTaskSchedule(DAILY_TASK &daily_task) {
   if (Firebase.RTDB.getString(&fbdo, "Daily_Task/Hour_on")) {
-    taskHour_on = atoi(fbdo.stringData().c_str());
+    daily_task.taskHour_on = atoi(fbdo.stringData().c_str());
     Serial.print("Hour_on: ");
-    Serial.println(taskHour_on);
+    Serial.println(daily_task.taskHour_on);
   }
 
   if (Firebase.RTDB.getString(&fbdo, "Daily_Task/Minutes_on")) {
-    taskMinute_on = atoi(fbdo.stringData().c_str());
+    daily_task.taskMinute_on = atoi(fbdo.stringData().c_str());
     Serial.print("Minutes_on: ");
-    Serial.println(taskMinute_on);
+    Serial.println(daily_task.taskMinute_on);
   }
 
   if (Firebase.RTDB.getString(&fbdo, "Daily_Task/Hour_off")) {
-    taskHour_off = atoi(fbdo.stringData().c_str());
+    daily_task.taskHour_off = atoi(fbdo.stringData().c_str());
     Serial.print("Hour_off: ");
-    Serial.println(taskHour_off);
+    Serial.println(daily_task.taskHour_off);
   }
 
   if (Firebase.RTDB.getString(&fbdo, "Daily_Task/Minutes_off")) {
-    taskMinute_off = atoi(fbdo.stringData().c_str());
+    daily_task.taskMinute_off = atoi(fbdo.stringData().c_str());
     Serial.print("Minutes_off: ");
-    Serial.println(taskMinute_off);
+    Serial.println(daily_task.taskMinute_off);
   }
 }
 
-void poll_id(uint8_t &slave_id) {
-  unsigned long now = millis();
-  if(now - prev > interval) {
-    if(slave_id == 1) {
-      zigbeeSerial.print("SLV_01\n");
-      Serial.println("Gui: SLV_01");
-      slave_id = 2;
-    } else if(slave_id == 2) {
-      zigbeeSerial.print("SLV_02\n");
-      Serial.println("Gui: SLV_02");
-      slave_id = 1;
+void poll_id(uint8_t &slave_id, buffer_t &buffer) {
+    static unsigned long prev = 0;
+    static uint16_t interval = 5000;
+    unsigned long now = millis();
+
+    if (now - prev > interval) {
+        if (slave_id == 1) {
+            zigbeeSerial.print("SLV_01\n");
+            Serial.println("Gui: SLV_01");
+            slave_id = 2;
+        } else if (slave_id == 2) {
+            zigbeeSerial.print("SLV_02\n");
+            Serial.println("Gui: SLV_02");
+            slave_id = 1;
+        }
+        prev = now;
+        buffer.index = 0;
+        buffer.data[0] = '\0';
     }
-    prev = now;
-    //Xoa toan bo data cu de tranh bi lap!
-    buf_index = 0;
-    buffer[0] = '\0';
-  } 
 }
 
