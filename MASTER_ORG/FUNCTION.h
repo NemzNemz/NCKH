31bfd05bae7f9126e2edcd8ede2338e9c2fd1fb2
NemzNemz <urgot0933@gmail.com>
2025-05-04 00:22:32 +0700

    Them 1 prototype

--- MASTER_ORG/FUNCTION.h
+++ MASTER_ORG/FUNCTION.h
@@ -8,9 +8,10 @@
 
 void poll_id(uint8_t &slave_id, buffer_t &buffer);
 void tokenStatusCallback(token_info_t info);
-void readFirebaseData(peripheral *pin, status_var *status);
+void readFirebaseData(peripheral *pin, control_status *status);
 void readDailyTaskSchedule(DAILY_TASK &daily_task);
 void send_value_to_firebase(timing_variables *timing, last_data_value *data);
-void send_state_to_firebase(status_var *status);
+void send_state_to_firebase(control_status *status);
+String gate_status(float ppm_sea, float ppm_river);
 
 #endif // FUNCTION_H
