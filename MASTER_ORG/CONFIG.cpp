0ae986514a7df69234ac62ef2d9234bf06667345
NemzNemz <urgot0933@gmail.com>
2025-05-04 00:21:22 +0700

    Them thanh vien trong struct timing

--- MASTER_ORG/CONFIG.cpp
+++ MASTER_ORG/CONFIG.cpp
@@ -4,7 +4,7 @@ void peripheral_init(peripheral *pin){
   pin ->ZIGBEE_RX = 16;
   pin ->ZIGBEE_TX = 17;
   pin ->LED_PIN = 5;
-  pin ->BUTTON_PIN = 32;
+  pin ->BUTTON_PIN = 15;
   pin ->ENABLE = 25;
   pin ->MOTOR_PIN1 = 27;
   pin ->MOTOR_PIN2 = 26;
