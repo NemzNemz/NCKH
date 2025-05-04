62be8a3cf0d0ab1c381fe02f01b4e53ebdffce8f
NemzNemz <urgot0933@gmail.com>
2025-05-04 22:09:37 +0700

    Tach hang loat ham tu main sang

--- MASTER_ORG/FUNCTION.cpp
+++ MASTER_ORG/FUNCTION.cpp
@@ -5,10 +5,14 @@ extern FirebaseData fbdo;
 extern FirebaseAuth auth;
 extern FirebaseConfig config;
 extern bool signUpOK;
+extern bool  frameOn;
+extern size_t frameIdx;
+extern char frameBuf[64];
+extern last_data_value data;
 
 void reconnect_firebase() {
     if (!Firebase.ready() && signUpOK) {
-        Serial.println("Thử kết nối lại Firebase...");
+        Serial.println("Tru to reconnect!...");
         Firebase.begin(&config, &auth);
         Firebase.reconnectWiFi(true);
     }
@@ -146,5 +150,74 @@ String gate_status(float ppm_sea, float ppm_river) {
             return "MỞ";
         }
     }
+}
+
+void nhan_data(buffer_t &buffer) {
+  while (zigbeeSerial.available()) {
+    char c = zigbeeSerial.read();
+    if (c == '\r') continue;
+
+    if (!frameOn) {
+      if (c == '<') {
+        frameOn = true;
+        frameIdx = 0;
+        frameBuf[frameIdx++] = c;
+      }
+      continue;
+    }
+
+    if (frameIdx < sizeof(frameBuf) - 1) {
+      frameBuf[frameIdx++] = c;
+    } else {
+      frameOn = false;
+      frameIdx = 0;
+      continue;
+    }
+
+    if (c == '\n') {
+      frameBuf[frameIdx] = '\0';
+      Serial.println(frameBuf);
+
+      if (strncmp(frameBuf, "<S1,", 4) == 0) SLV1_data(frameBuf, data);
+      else if (strncmp(frameBuf, "<S2,", 4) == 0) SLV2_data(frameBuf, data);
+      else Serial.println("Khong nhan dien duoc slave_id");
+
+      frameOn = false;
+      frameIdx = 0;
+      buffer.index = 0;
+    }
+  }
+}
+
+void SLV2_data(const char *s, last_data_value &data) {
+  float wtr, ph, tds;
+  if (sscanf(s, "<S2,WTR=%f,PH=%f,TDS=%f>", &wtr, &ph, &tds) == 3) {
+    data.last_WTR_SLV2 = wtr;
+    data.last_PH_SLV2  = ph;
+    data.last_TDS_SLV2 = tds;
+  }
+}
+
+void SLV1_data(const char *s, last_data_value &data) {
+  float wtr, ph, tds;
+  if (sscanf(s, "<S1,WTR=%f,PH=%f,TDS=%f>", &wtr, &ph, &tds) == 3) {
+    data.last_WTR_SLV1 = wtr;
+    data.last_PH_SLV1  = ph;
+    data.last_TDS_SLV1 = tds;
+  }
+}
+
+void handle_gate_state_and_direction_by_firebase(control_status &status, int &motor_direction, timing_variables *timing, int &previous_motor_status){
+    if (status.motor_status != previous_motor_status) {
+      if (status.motor_status == 1) {
+          motor_direction = MOTOR_OPENING; // Quay thuận
+          Serial.println("Huong dong co (Firebase): Thuan");
+      } else {
+          motor_direction = MOTOR_CLOSING; // Quay nghịch
+          Serial.println("Huong dong co (Firebase): Nghich");
+      }
+      timing->motor_start_time = 0;
+      previous_motor_status = status.motor_status;
+  }
 }
 
